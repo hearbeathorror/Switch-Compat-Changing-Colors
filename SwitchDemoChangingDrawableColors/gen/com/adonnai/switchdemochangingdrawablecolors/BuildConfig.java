f7b32ce62b668d552ce7c284236bcb0ba6b7e020
hearbearhorror <hearbeat.horror@gmail.com>
2014-02-16 10:34:45 +0530

    initial commit, examples of switch colors as per the application

---
+++ SwitchDemoChangingDrawableColors/gen/com/adonnai/switchdemochangingdrawablecolors/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.adonnai.switchdemochangingdrawablecolors;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
